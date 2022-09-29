8569567c2b5733c2ce09046e1e8727fd2c429944
Liaquat1207 <liaquat55678@gmail.com>
2022-09-29 11:56:45 +0500

    Website

---
+++ js/Add-on.js
@@ -0,0 +1,37 @@
+// Active Navbar Start
+$(document).ready(function(){
+    $('ul li a').click(function(){
+      $('li a').removeClass("active");
+      $(this).addClass("active");
+  });
+});
+
+// Active Navbar End
+
+// Get Location
+$(document).ready(function(){
+    $('.get-btn').click(function(){
+        $('.remove').hide();
+        $('.add').show();
+  });
+});
+
+// Flying Image
+$(document).ready(function(){
+    setTimeout(function() { 
+        $("#myModal").modal();
+    }, 1500);
+});
+
+// Mobile Navbar Start
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+// Mobile Navbar End
+
